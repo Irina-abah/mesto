b27ccf090a464cdce5647b80a66d040c0e08c766
Irina Abah <irin.bon@gmail.com>
2020-10-27 15:37:49 +0000

    feat: добавить логику для открытия и закрытия попапа

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+const popup = document.querySelector('.popup');
+const popupOpened = document.querySelector('.popup_opened');
+
+const popupEditButton = document.querySelector('.button_type_edit');
+const popupCloseButton = document.querySelector('.button_type_close');
+
+popupEditButton.addEventListener('click', true);
+
+function popup() {
+    if () {
+
+    }
+    else {
+        popup.classList.remove('.popup_opened')
+    }
+    
+}
+
