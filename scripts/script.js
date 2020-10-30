1d223ed82cf1f03d600455495bc10d7fa9ed8f3a
Irina Abah <irin.bon@gmail.com>
2020-10-30 15:51:08 +0000

    fix: исправить замечания после первой проверки

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,16 @@ let popup = document.querySelector('.popup');
 let popupOpened = document.querySelector('.popup_opened');
 let popupEditButton = document.querySelector('.button_type_edit');
 let popupCloseButton = document.querySelector('.button_type_close');
-
-popupEditButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
+let formElement = document.querySelector('.popup__input-container');
+let profileName = document.querySelector('.profile__name');
+let profileTitle = document.querySelector('.profile__title');
+let inputName = document.querySelector('#profile-name');
+let inputTitle = document.querySelector('#profile-title');
 
 function openPopup() {
     popup.classList.add('popup_opened');
+    inputName.value = profileName.textContent;
+    inputTitle.value = profileTitle.textContent;
 }
 
 function closePopup() {
@@ -16,15 +20,6 @@ function closePopup() {
 }
 
 // редактирование формы
-
-let formElement = document.querySelector('.popup__input-container');
-let profileName = document.querySelector('.profile__name');
-let profileTitle = document.querySelector('.profile__title');
-let inputName = document.querySelector('#profile-name');
-let inputTitle = document.querySelector('#profile-title');
-
-inputName.value = profileName.textContent;
-inputTitle.value = profileTitle.textContent;
 
 function formSubmit (evt) {
     evt.preventDefault();
@@ -34,5 +29,7 @@ function formSubmit (evt) {
     profileTitle.textContent = inputTitle.value;
 }
 
+popupEditButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmit);
 
