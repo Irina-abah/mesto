3e43b64f9e4a95fe3fb18ded4464565ddb06342a
Irina Abah <irin.bon@gmail.com>
2020-11-16 00:19:47 +0000

    refactor:  re-write close popup function

--- scripts/script.js
+++ scripts/script.js
@@ -1,46 +1,37 @@
+// кнопки
 
-let popupEditButton = document.querySelector('.button_type_edit');
-let popupCloseButton = document.querySelector('.button_type_close');
-let popupAddButton = document.querySelector('.button_type_add');
-let formElement = document.querySelector('.popup__input-container');
+const popupEditButton = document.querySelector('.button_type_edit');
+const popupCloseButtons = document.querySelectorAll('.button_type_close');
+const popupAddButton = document.querySelector('.button_type_add');
+
+const formElement = document.querySelector('.popup__input-container');
 let profileName = document.querySelector('.profile__name');
 let profileTitle = document.querySelector('.profile__title');
+
 let inputName = document.querySelector('#profile-name');
 let inputTitle = document.querySelector('#profile-title');
 let elementTitle = document.querySelector('#element-title');
 let elementImage = document.querySelector('#element-image');
-let editProfilePopup = document.querySelector('.popup_type_edit');
-let addCardPopup = document.querySelector('.popup_type_add');
-let popup = document.querySelector('.popup');
-let popupTitle = document.querySelectorAll('.popup__title');
+
+const editProfilePopup = document.querySelector('.popup_type_edit');
+const addCardPopup = document.querySelector('.popup_type_add');
+let popupTitles = document.querySelectorAll('.popup__title');
 
 
-// открытие и закрытие попапа
+// открытие попапа EditProfile
 
-// function openPopup() {
-//     popup.classList.add('popup_opened');
-// }
+function openEditProfilePopup() {
+    editProfilePopup.classList.add('popup_opened');
+    }
 
+// открытие попапа addCard
 
-function closePopup() {
-    popup.classList.remove('popup_opened');
-}
+function openAddCardPopup() {
+    addCardPopup.classList.add('popup_opened');
+    }
+
 
 // редактирование профиля
-
-function editProfile() {
-
-    popupEditButton.addEventListener('click', () => {
-        editProfilePopup.classList.add('popup_opened');
-    });
-    
-        inputName.value = profileName.textContent;
-        inputTitle.value = profileTitle.textContent;
-     
-}
-editProfile();
-
-// добавление картинки
 
 function editProfile() {
         
@@ -50,16 +41,48 @@ function editProfile() {
 }
 editProfile();
 
-
 function formSubmit (evt) {
     evt.preventDefault();
-    closePopup();
+    
 
     profileName.textContent = inputName.value;
     profileTitle.textContent = inputTitle.value;
+    closePopup();
 }
 
 
-// popupAddButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
+popupAddButton.addEventListener('click', openAddCardPopup);
+popupEditButton.addEventListener('click', openEditProfilePopup);
+// popupCloseButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmit);
+
+// popupCloseButtons.addEventListener('click', evt => {
+
+//     popupCloseButtons.Array.classList.remove('popup_opened');
+
+//     else
+// });
+
+//     popupCloseButtons.forEach(function(btn) {
+//         btn.addEventListener('click', function(evt) {
+//             if (evt.target.classList.contains('popup_opened')) {
+//                     .classList.remove('popup_opened');
+//             }
+//         })
+//     })
+// }
+// closePopup();
+
+function closePopup() {
+   popupCloseButtons.forEach(function(btn) {
+       btn.addEventListener('click', function(evt) {
+               evt.preventDefault();
+              
+               const popup = evt.target.closest('.popup');
+               popup.classList.remove('popup_opened');
+    
+       })
+   })
+}
+
+closePopup();
