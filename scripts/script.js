5eb3d43c3163d310c57eb78a87b48483139ee1d4
Alexander.Voloschenko <alexander@socialedge.me>
2020-12-09 12:04:23 +0200

    add IIFE

--- scripts/script.js
+++ scripts/script.js
@@ -37,68 +37,68 @@ function editProfile() {
     openPopup(editProfilePopup);
     inputName.value = profileName.textContent;
     inputTitle.value = profileTitle.textContent;
-    }
+}
 
 editFormElement.addEventListener('submit', (evt) => {
-        evt.preventDefault();
-        
-        profileName.textContent = inputName.value;
-        profileTitle.textContent = inputTitle.value;
-        
+    evt.preventDefault();
+
+    profileName.textContent = inputName.value;
+    profileTitle.textContent = inputTitle.value;
+
 });
 
 // закрыть любой попап при нажатии на крестик и при сабмите
 
 function closePopup(popup) {
     popup.classList.remove('popup_opened');
-    
 }
 
 function setClosePopupListeners() {
-       
-    popupCloseButtons.forEach(function(form) {
-            form.addEventListener('click', function(evt) {
-                const popup = evt.target.closest('.popup');
-                closePopup(popup);
+    popupCloseButtons.forEach(function (form) {
+        form.addEventListener('click', function (evt) {
+            const popup = evt.target.closest('.popup');
+            closePopup(popup);
         });
     });
 
-     inputForms.forEach(function(form) {
-        form.addEventListener('submit', function(evt) {
+    inputForms.forEach(function (form) {
+        form.addEventListener('submit', function (evt) {
             evt.preventDefault();
             const popup = evt.target.closest('.popup');
             closePopup(popup);
         });
     });
 
-    allPopups.forEach(function(popup) {
-        popup.addEventListener('mousedown', function(evt) {
+    allPopups.forEach(function (popup) {
+        popup.addEventListener('mousedown', function (evt) {
             if (evt.target.classList.contains('popup')) {
-                closePopup(popup); 
-                
-            };   
+                closePopup(popup);
+
+            }
+            ;
         });
     });
 
-    
-    document.addEventListener('keydown', function(evt) {
+
+    document.addEventListener('keydown', function (evt) {
         if (evt.key === 'Escape') {
             allPopups.forEach((popup) => {
                 closePopup(popup);
             })
-        };    
+        }
+        ;
     });
- 
+
 };
 
-   
+
 setClosePopupListeners();
 
 // рендеринг карточек
 
 const initialCards = [
     {
-        
+
         name: 'Корраловый Риф',
         link: './images/grid-great-barrier-reef-australia.jpg',
         alt: 'Большой Корраловый Риф'
@@ -119,7 +119,7 @@ const initialCards = [
         alt: 'Вид сверху на бухту города Котор'
     },
     {
-        
+
         name: 'Исландия',
         link: './images/grid-seljalandsfoss-iceland.jpg',
         alt: 'Вид на водопад Селйяландсфосс'
@@ -129,7 +129,7 @@ const initialCards = [
         link: './images/grid-twelve-apostles-australia.jpg',
         alt: 'Пляж Двенадцать Апостолов в Австралии'
     }
-]; 
+];
 
 function deleteCard(evt) {
     const deletedCard = evt.target.closest('.place');
@@ -152,36 +152,36 @@ function createNewCard(card) {
     const cardLikeButton = cardElement.querySelector('.place__like');
     const templateCardName = cardElement.querySelector('.place__title');
     const templateCardImage = cardElement.querySelector('.place__image');
-    templateCardImage.addEventListener('click', () => 
-    previewImage(card));
+    templateCardImage.addEventListener('click', () =>
+        previewImage(card));
     cardDeleteButton.addEventListener('click', deleteCard);
     cardLikeButton.addEventListener('click', likeCard);
-  
+
     templateCardName.textContent = card.name;
     templateCardImage.src = card.link;
     templateCardImage.alt = card.alt;
-    
+
     return cardElement;
-}; 
+};
 
 initialCards.forEach((item) => {
     const newCard = createNewCard(item)
     cards.append(newCard);
 });
 
-addFormElement.addEventListener('submit', function(evt) {
-        
-        const templateCardName = placeInputTitle.value;
-        const templateCardImage = placeInputLink.value;
+addFormElement.addEventListener('submit', function (evt) {
 
-        const newCard = createNewCard ({
-            name: templateCardName, 
-            link: templateCardImage
-        }); 
-        
-        cards.prepend(newCard);
-        addFormElement.reset();
-    })
+    const templateCardName = placeInputTitle.value;
+    const templateCardImage = placeInputLink.value;
+
+    const newCard = createNewCard({
+        name: templateCardName,
+        link: templateCardImage
+    });
+
+    cards.prepend(newCard);
+    addFormElement.reset();
+})
 
 popupAddButton.addEventListener('click', function () {
     openPopup(addPlacePopup);
@@ -210,17 +210,17 @@ function checkInputValidity(form, input, config) {
         showInputError(form, input, config);
     } else {
         hideInputError(form, input, config);
-    };
+    }
 };
 
 function toggleButtonState(button, isActive, config) {
-     if(!isActive) {
-         button.classList.add(config.inactiveButtonClass);
-         button.disabled = true;
-     } else {
+    if (!isActive) {
+        button.classList.add(config.inactiveButtonClass);
+        button.disabled = true;
+    } else {
         button.classList.remove(config.inactiveButtonClass);
         button.disabled = false;
-     };
+    }
 };
 
 function setEventListeners(form, config) {
@@ -233,7 +233,6 @@ function setEventListeners(form, config) {
             toggleButtonState(submitButton, form.checkValidity(), config);
         });
     });
-        
 };
 
 function enableValidation(config) {
@@ -257,6 +256,6 @@ const config = {
     submitButtonSelector: '.button_type_submit',
     inactiveButtonClass: 'button_type_submit-disabled',
     errorClass: '.input__error'
-  };
+};
 
-  enableValidation(config);
+enableValidation(config);
